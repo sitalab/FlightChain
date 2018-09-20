794dc9285ef0e8d6616e2c07a0e7016e6a653ef5
Kevin Osullivan <kevin.osullivan@sita.aero>
2018-09-20 10:12:26 +0100

    Migrated all code to gitlab.
    Signed-off-by: Kevin Osullivan <kevin.osullivan@sita.aero>

--- chaincode/src/index.ts
+++
@@ -1,5 +0,0 @@
-import shim = require('fabric-shim');
-import { FlightChain } from './flightChain';
-
-// FlightChain Chaincode is moved to seperate file for testing
-shim.start(new FlightChain());
--- flight-chain-api/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import {FlightChainModule} from "./flight-chain/flight-chain.module";
-
-@Module({
-  imports: [FlightChainModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
